a34d2a172a79c009cb41fed51fa837d6782af010
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-03-09 12:16:28 +0800

    ✨ feat(init): initialize branch 'main'
    Initialize the 'main' branch with the following files:
    
    - Add document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Add GitHub workflow files: ci-check-pr-status,..., etc.
    - Add GitHub issue templates: language_request.yml,..., etc.
    - Add Git config files: .gitmodules, .gitignore.
    - Add Git submodules: cmake/modules, cmake/plugins.
    - Add CMake script files: CMakeLists.txt, cmake/**/* files.
    - Add config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["latest",  "Latest Release"],
+    ["2",       "Release 2.x"],
+    ["1",       "Release 1.x"],
+    ["0",       "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.ltdorgtest.org"],
+    ["Crowdin", "https://ltdorgtest.crowdin.com/scrapy-docs-l10n"],
+    ["GitHub",  "https://github.com/ltdorgtest/scrapy-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/ltdorgtest/scrapy-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/ltdorgtest/scrapy-docs-l10n"],
+  ]
+};
