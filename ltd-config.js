7165a80aa0928dd5278f70846be4e417842f9236
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:27:56 +0000

    Deploying to pages from @ localizethedocs/scrapy-docs-l10n@9a4503dcb3729588e48f8d7b44ab147971225bc1 🚀

---
+++ ltd-config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["latest",  "Latest Release"],
+    ["2",       "Release 2.x"],
+    ["1",       "Release 1.x"],
+    ["0",       "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/scrapy-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/scrapy-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/scrapy-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/scrapy-docs-l10n"],
+  ]
+};
